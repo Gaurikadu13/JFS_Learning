7b969e87e3153cf6209df9df7933dc46d58e7d04
Gaurikadu13 <gaurikadu324@gmail.com>
2026-03-21 05:16:51 +0300

    Day 1: Write first code

---
+++ Core Java/Arith_oprn.java
@@ -0,0 +1,10 @@
+public class Arith_oprn
+{
+    public static void main(String args[])
+    {
+        System.out.println("Addition : "+(4+5));
+        System.out.println("Substarction : "+(11-5));
+        System.out.println("Multiplication : "+(3*5));
+        System.out.println("Division : "+(15/3));
+    }
+}
